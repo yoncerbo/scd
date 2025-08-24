b85c249dd48353dfb381005ddd58428405d35936
meersen <>
2025-08-24 11:43:06 +0200

    Added more operations to the alu

--- src/alu.sv
+++ src/alu.sv
@@ -1,13 +1,22 @@
-`include "full_adder.sv"
+module ALU (input [15:0] a, b, input ci, nb, ic, zb, output [15:0] out, output co);
 
-module ALU (input [15:0] a, b, input ci, nb, output [15:0] out);
+wire [15:0] carry_in, carry_out, carry, x, y;
 
-wire [16:0] carry;
+assign carry_in[0] = ci;
+assign x = a;
 
-assign carry[0] = ci;
+for (genvar i = 0; i < 16; i = i + 1) begin
+  assign y[i] = (b[i] & ~zb) ^ nb;
+  assign carry[i] = carry_in[i] & ~ic;
+end
 
-for (genvar i = 0; i <= 15; i = i + 1) begin
-  FullAdder fa(a[i], b[i] ^ nb, carry[i], out[i], carry[i + 1]);
+assign out = x ^ y ^ carry;
+assign carry_out = (x & y) | ((x ^ y) & carry);
+
+assign co = carry_out[15];
+
+for (genvar i = 0; i < 15; i = i + 1) begin
+  assign carry_in[i + 1] = carry_out[i];
 end
 
 endmodule;
--- src/tb_alu.sv
+++ src/tb_alu.sv
@@ -3,20 +3,23 @@
 
 module tb_alu;
 
-reg ci, nb;
+reg ci, nb, ic, zb;
+wire co;
 reg [15:0] a, b;
 wire [15:0] out;
 wire signed [15:0] sout;
 
 assign sout = out;
 
-ALU alu(a, b, ci, nb, out);
+ALU alu(a, b, ci, nb, ic, zb, out, co);
 
 initial begin
-
-  // Addition
   ci = 0;
   nb = 0;
+  ic = 0;
+  zb = 0;
+
+  // Addition
   a = 9;
   b = 8;
   #1 `ASSERT(out, 17);
@@ -32,9 +35,15 @@ initial begin
   #1 `ASSERT(sout, -2);
 
   // Biggest unsigned number in out
-  a = 65533;
+  a = 65534;
   b = 1;
-  #1 `ASSERT(out, 65534);
+  #1 `ASSERT(out, 65535);
+
+  // Overflow
+  a = 65534;
+  b = 2;
+  #1 `ASSERT(out, 0);
+  `ASSERT(co, 1);
 
   // Subtraction
   ci = 1;
@@ -43,9 +52,38 @@ initial begin
   b = 4;
   #1 `ASSERT(out, 6);
 
+  // Xor
+  ci = 0;
+  nb = 0;
+  ic = 1;
+  a = 10;
+  b = 9;
+  #1 `ASSERT(out, 3);
 
+  // Increment
+  ci = 1;
+  nb = 0;
+  ic = 0;
+  zb = 1;
+  a = 16;
+  #1 `ASSERT(out, 17);
 
-  // Subtraction
+  // Decrement
+  ci = 0;
+  nb = 1;
+  ic = 0;
+  zb = 1;
+  a = 16;
+  #1 `ASSERT(out, 15);
+
+  // TODO: And
+  // TODO: Or
+  // TODO: Not
+  // TODO: Negate
+  // TODO: Shift left
+  // TODO: Shift right
+  // TODO: Shift right signed
+
   $finish;
 end
 
