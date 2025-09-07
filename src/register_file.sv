2006386fa0315556d3afb1b21cdd999ebbc1f530
meersen <>
2025-09-07 03:46:42 +0200

    Fixed register file

--- src/register_file.sv
+++ src/register_file.sv
@@ -1,8 +1,8 @@
 `include "gated_latch.sv"
 
-module Register (input we, input [15:0] d_in, output [15:0] d_out);
+module Register (input we, input [7:0] d_in, output [7:0] d_out);
 
-for (genvar i = 0; i < 16; i = i + 1) begin
+for (genvar i = 0; i < 8; i = i + 1) begin
   GatedLatch latch(we, d_in[i], d_out[i]);
 end
 
@@ -11,20 +11,14 @@ endmodule;
 module RegisterFile (
   input we,
   input [3:0] sel_in, sel_o1, sel_o2,
-  input [15:0] in,
-  output [15:0] o1, o2);
+  input [7:0] in,
+  output [7:0] o1, o2);
 
-wire [15:0] [15:0] outs;
+wire [7:0] outs [15:0];
 
-wire [3:0] sig1, sig2;
-Decoder dec1(sel_in[1:0], sig1);
-Decoder dec2(sel_in[3:2], sig2);
-
-for (genvar i = 0; i < 4; i = i + 1) begin
-  for (genvar j = 0; j < 4; j = j + 1) begin
-    assign enable = sig1[j] & sig2[i] & we;
-    Register register(enable, in, outs[4 * i + j]);
-  end
+for (genvar i = 0; i < 16; i = i + 1) begin
+  assign enable = (sel_in == i) & we;
+  Register register(enable, in, outs[i]);
 end
 
 assign o1 = outs[sel_o1];
